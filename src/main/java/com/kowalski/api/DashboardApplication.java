01d8f5dfcebadd93e6c01cd2d13973e7875d98e0
paavosoeiro <paavomsoeiro@gmail.com>
2022-05-18 14:35:44 -0300

    Upload dashboard repo

---
+++ src/main/java/com/kowalski/api/DashboardApplication.java
@@ -0,0 +1,19 @@
+package com.kowalski.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+@EnableEurekaClient
+public class DashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardApplication.class, args);
+	}
+
+}
